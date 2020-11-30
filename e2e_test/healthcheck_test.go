b36084645764a0c6c00252b69d43408e106a4fbb
Mark Bradley <braddle@gmail.com>
2020-11-30 20:46:01 +0000

    Creating the Health Check endpoint

--- e2e_test/healthcheck_test.go
+++ e2e_test/healthcheck_test.go
@@ -2,6 +2,8 @@ package e2e_test
 
 import (
 	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -13,6 +15,14 @@ func TestEndToEndSuite(t *testing.T) {
 	suite.Run(t, new(EndToEndSuite))
 }
 
-func (s *EndToEndSuite) TestPlaceholder() {
-	s.True(true)
+func (s *EndToEndSuite) TestHappyHealthcheck() {
+	c := http.Client{}
+
+	r, _ := c.Get("http://localhost:8080/healthcheck")
+
+	s.Equal(http.StatusOK, r.StatusCode)
+
+	b, _ := ioutil.ReadAll(r.Body)
+
+	s.JSONEq(`{"status": "OK", "messages": []}`, string(b))
 }
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 	"time"
 )
 
+type health struct {
+	Status string `json:"status"`
+	Messages []string `json:"messages"`
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	r.HandleFunc(
+		"/healthcheck",
+		func(w http.ResponseWriter, r *http.Request) {
+			h := health{
+				Status:   "OK",
+				Messages: []string{},
+			}
+
+			b, _ := json.Marshal(h)
+
+			w.Write(b)
+			w.WriteHeader(http.StatusOK)
+	})
 
 	s := http.Server{
 		Addr:              ":8080",
